ece978d78afc0cb6c1fbf37d29931506c608abd6
diegoram23 <diego.pta.oc@gmail.com>
2023-05-20 18:51:15 -0700

    Appropriate text box is generated based off single choice parameters from user for Ther Ex box

---
+++ data.js
@@ -0,0 +1,23 @@
+export const therExData = [
+    {
+        supineExText: `Instruction in supine LE AROM heel slides, hip abd, SLR's, ankle pumps, and bridging x10 each 
+    to increase strength and ROM needed for functional activities with cues to correct technique.`
+    },
+
+    {
+        seatedExText: `Instruction in seated LE AROM LAQ's, hip abd, ankle pumps, marching, and ball squeeze for hip add x10 each
+    to increase strength and ROM needed for functional activities with cues to correct technique.`,
+
+    },
+
+    {
+        standingEx: `Instruction in standing LE AROM heel raises, hip abd, marching, squats, and hip ext x10 
+    each to increase strength and ROM needed for functional activities with cues to correct technique.`
+    },
+
+    {
+        weights: `Exercises does with for RET.`
+    }
+
+]
+
--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+import { therExData } from "/data.js"
+
 const createBtn = document.getElementById('create-btn')
+const supineEx = document.getElementById('supine-ex')
+const seatedEx = document.getElementById('seated-ex')
+const standingEx = document.getElementById('standing-ex')
+const therExText = document.getElementById('ther-ex-box')
+
+let therExArray = []
 
 createBtn.addEventListener('click', () => {
-    console.log('clicked')
+
+    if (supineEx.checked) {
+        therExArray.push(Object.values(therExData[0]))
+        therExText.textContent = therExArray
+    }
+
+    if (seatedEx.checked) {
+        therExArray.push(Object.values(therExData[1]))
+        therExText.textContent = therExArray
+    }
+
+    if (standingEx.checked) {
+        therExArray.push(Object.values(therExData[2]))
+        therExText.textContent = therExArray
+    }
+
+    // if (seatedEx.checked && supineEx.checked) {
+    //     therExArray.push((Object.values(therExData[2]) + Object.values(therExData[1])))
+    //     removeDuplicate(therExArray)
+    // }
+
 })
+
+function removeDuplicate(str) {
+    let isDuplString = JSON.stringify(str)
+    // console.log(isDuplString.split(''))
+}
